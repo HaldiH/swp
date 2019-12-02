ad6c809af18373b062a44a511ed16fe156cd23bb
ShinoYasx <20846785+ShinoYasx@users.noreply.github.com>
2019-12-02 22:15:49 +0100

    Cleaned

--- src/db_utils.hpp
+++ src/db_utils.hpp
@@ -5,15 +5,20 @@
 
 #define DBNAME "server.db"
 
-namespace SWP {
+namespace swp {
     struct SecValue {
         std::vector<std::string> value;
         int sqlite_code;
     };
 
-    class server {
+    class ServerDB {
     public:
-        server() {
+        ServerDB() {
+            int rc;
+            rc = sqlite3_open(DBNAME, &db);
+            if (rc)
+                std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
+
             const std::string sql = "CREATE TABLE IF NOT EXISTS `users` ("
                                     "`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
                                     "`username` TEXT NOT NULL,"
@@ -23,22 +28,24 @@ namespace SWP {
                                     "`owner` TEXT NOT NULL,"
                                     "`group` TEXT,"
                                     "`value` TEXT NOT NULL );";
-            exec_request(sql);
+            rc = exec_request(sql);
+            if (rc !=  SQLITE_OK)
+                std::cerr << "Can't initialize tables: " << sqlite3_errmsg(db) << std::endl;
         }
 
-        static SecValue getToken(const std::string &username) {
+        SecValue getToken(const std::string &username) {
             const std::string sql = "SELECT `token` FROM users WHERE username='" + username + "'";
-            return select_request(sql);
+            return select_request(sql, 0);
         }
 
-        static int setToken(const std::string &token, const std::string &username) {
+        int setToken(const std::string &token, const std::string &username) {
             const std::string sql = "INSERT INTO users ('username','token') "
                                     "VALUES ('" +
                                     username + "','" + token + "');";
             return exec_request(sql);
         }
 
-        static int storePassword(const std::string &value, const std::string &username, const std::string &group) {
+        int storePassword(const std::string &value, const std::string &username, const std::string &group) {
             const std::string sql = "INSERT INTO `passwords` ('owner','value','group') "
                                     "VALUES ('" +
                                     username + "', '" + value + "', '" + group + "');";
@@ -46,62 +53,40 @@ namespace SWP {
             return exec_request(sql);
         }
 
-        static SecValue getUserPasswords(const std::string &username) {
+        SecValue getUserPasswords(const std::string &username) {
             const std::string sql = "SELECT value FROM passwords WHERE `owner`='" + username + "';";
 
-            SecValue value = select_request(sql);
+            SecValue value = select_request(sql, 0);
             return value;
         }
 
     private:
-        static sqlite3 *openDataBase() {
-            sqlite3 *db;
-            int rc;
+        sqlite3 *db{};
 
-            rc = sqlite3_open(DBNAME, &db);
-
-            if (rc) {
-                std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
-                return nullptr;
-            }
-            return db;
-        }
-
-        static int exec_request(const std::string &sql) {
-            sqlite3 *db = openDataBase();
-            if (!db)
-                return SQLITE_ERROR;
-
+        int exec_request(const std::string &sql) {
             char *zErrMsg;
 
             int rc = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &zErrMsg);
             if (rc != SQLITE_OK)
                 std::cerr << zErrMsg << std::endl;
-            sqlite3_close_v2(db);
             return rc;
         }
 
-        static SecValue select_request(const std::string &sql) {
-            sqlite3 *db = openDataBase();
-            if (!db)
-                return SecValue{std::vector<std::string>{}, SQLITE_ERROR};
-
+        SecValue select_request(const std::string &sql, const int iCol) {
             int rc;
             sqlite3_stmt *stmt = nullptr;
             rc = sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr);
             if (rc != SQLITE_OK) {
-                std::cerr << "Prepare failed" << std::endl;
-                sqlite3_close(db);
+                std::cerr << sqlite3_errmsg(db) << std::endl;
                 return SecValue{std::vector<std::string>{}, SQLITE_ERROR};
             }
             std::vector<std::string> vector{};
             while ((rc = sqlite3_step(stmt)) == SQLITE_ROW)
-                vector.emplace_back((reinterpret_cast<const char *>(sqlite3_column_text(stmt, 0))));
+                vector.emplace_back((reinterpret_cast<const char *>(sqlite3_column_text(stmt, iCol))));
 
             SecValue value = {vector, rc};
 
             sqlite3_finalize(stmt);
-            sqlite3_close(db);
             return value;
         }
     };
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 #include "db_utils.hpp"
 
 int main(int argc, char *argv[]) {
-    SWP::server server;
-    SWP::server::setToken("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "test");
-    SWP::server::storePassword("Hello, World!", "test", "");
-    for (const auto &v : SWP::server::getUserPasswords("test").value) {
+    swp::ServerDB db;
+    db.setToken("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "test");
+    db.storePassword("Hello, World!", "test", "");
+    for (const auto &v : db.getUserPasswords("test").value) {
         std::cout << "Password: " << v << std::endl;
     }
-    for (const auto &v: SWP::server::getToken("test").value) {
+    for (const auto &v: db.getToken("test").value) {
         std::cout << "Token: " << v << std::endl;
     }
 }
